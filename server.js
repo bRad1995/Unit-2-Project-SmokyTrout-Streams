6042d3cf195cbf89109b00712040b9b20b7ce6c8
bRad1995 <bradleejwilliams@yahoo.com>
2023-11-21 20:15:43 -0600

    Show route and show page working

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/smokytrout/seed', (req, res) => {
             fishing_quality: 'Fair to Good',
             accessibility: 'Easy to Moderate',
             recommended_flies: 'Little Yellow Sally, Blue Winged Olive, Green Weenie',
-            discription: "This is a wonderful small stream if you're looking for some native Smoky Mountain brook trout but don't want to commit to the extreme backwoods hiking that comes to finding them. Unlike other brook trout streams in park; this one is relativly easy to access, with the trail coming in contact with the stream only 0.5 miles from The Sinks where the trail head begins (Meigs Creek Trail). Once from there, the trail weaves over and around stream in several locations until reaching the fork of Henderson's Prong and Bunch Prong. From there the trail follows down Bunch Prong until leaving the stream entirely to merge with Meigs Moutain Trail. The small hike to reach this stream can be very rewarding for anglers looking to get away from the crowds without having to go too deep into the woods; that is... if you're willing to tackle the 500 foot incline before decending down to the stream on the Miegs Creek Trail."
+            discription: "This is a wonderful small stream if you're looking for native Smoky Mountain brook trout, but don't want to commit to the extreme backwoods hiking to find them. Unlike other brook trout streams in the park; this one is relativly easy to access, with the trail coming in contact with the stream only 0.5 miles from The Sinks where the trail head begins (Meigs Creek Trail). Once there, the trail weaves over and around stream in several locations until reaching the fork of Henderson's Prong and Bunch Prong. From there the trail follows down Bunch Prong until leaving the stream entirely to merge with Meigs Moutain Trail. The small hike to reach this stream can be very rewarding for anglers looking to get away from the crowds without having to go too deep into the woods; that is... if you're willing to tackle the 500 foot incline before decending down to the stream (a small feat comparied to hiking the 2 to 3 miles for almost every other brook trout stream in the park)."
         }
     ])
 })
@@ -32,6 +32,18 @@ app.get('/smokytrout', (req, res) => {
     Stream.find({}, (error, allStreams) =>{
         res.render('index.ejs', {
             streams : allStreams
+        })
+    })
+})
+
+//NEW
+
+
+//SHOW
+app.get('/smokytrout/:id', (req, res) => {
+    Stream.findById(req.params.id, (error, selectedStream) =>{
+        res.render('show.ejs', {
+            stream : selectedStream
         })
     })
 })
